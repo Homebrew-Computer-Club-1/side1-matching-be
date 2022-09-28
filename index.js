1b98d27528b185899e23e297acd1dfd35e32cb71
wonsun2006 <44808210+wonsun2006@users.noreply.github.com>
2022-09-28 23:23:23 +0900

    [22.09.28] Youtube API key 활용
    1. dotenv 모듈 추가
    2. Axios 모듈 추가
    3. Youtube API 에서 API key 가져와 공용 데이터 출력 확인

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+import { router } from './routes/routes';
+dotenv.config();
 const app = express();
-app.use("/router", require("./routes/routes"));
-app.listen('8080', function () {
+app.use("/router", router);
+app.listen(process.env.PORT, function () {
     console.log("listening to 8080");
 });
 app.get('/', function (req, res) {
     res.send('home');
+});
+app.get('/get-data', function (req, res) {
+    const config = {
+    /* Your settings here like Accept / Headers etc. */
+    };
+    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.API_KEY}`, config)
+    // axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=25&key=${process.env.API_KEY}`, config)
+        .then(function (response) {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+    });
+    res.redirect('/');
 });

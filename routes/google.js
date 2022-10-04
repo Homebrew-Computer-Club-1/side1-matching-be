6663b1770ca73fd929ab4d6cf9db6b47e92c6339
wonsun2006 <44808210+wonsun2006@users.noreply.github.com>
2022-10-04 23:55:15 +0900

    [22.10.04] MySQL 적용

--- routes/google.js
+++ routes/google.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import passport from 'passport';
+import dotenv from 'dotenv';
+dotenv.config();
 export const googleRouter = Router();
 googleRouter.get('/', passport.authenticate('google', {
     scope: ['profile', 'https://www.googleapis.com/auth/youtube']
 }));
 googleRouter.get('/callback', passport.authenticate('google', {
-    successRedirect: '../../',
+    successRedirect: process.env.CLIENT_REDIRECT_URL,
     failureRedirect: '/login-fail'
 }));
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,46 @@
 import express from 'express';
 import passport from 'passport';
 import session from 'express-session';
-import * as bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import axios, {AxiosResponse} from 'axios';
 import {googleRouter} from './routes/google';
 import {youtubeRouter} from './api/youtube_api';
 import passportConfig from './passport';
+import {connection} from './lib/mysql'
 
 dotenv.config();
 
 passportConfig();
 
 const app = express();
+const db = connection;
+
+declare global {
+    namespace Express{
+        interface User{
+            id:string|undefined;
+        }
+    }
+}
 
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
-  }))
+    cookie: { secure: false }
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.json())
 
 app.use("/auth/google", googleRouter);
 app.use("/youtube", youtubeRouter);
 
 app.listen(process.env.PORT, function(){
-    console.log("listening to 8080");
+    db.connect();
+    console.log(`listening to ${process.env.PORT}`);
 });
 
 app.get('/',function(req:express.Request, res:express.Response){
@@ -48,4 +60,27 @@ app.get('/get-data', function(req,res){
         console.log(response.config);
     });
     res.redirect('/')    
+});
+
+app.get('/get-google-id',function(req:express.Request, res:express.Response){
+    if(req.user != undefined){
+        console.log(req.user.id);
+        res.json({googleId: req.user.id});
+    }
+})
+
+app.post('/insert-user-data', function(req,res){
+    db.query(`INSERT INTO user_info VALUES(${req.body.googleId}, ${req.body.name}, ${req.body.age})`, function (error, results, fields) {
+        if (error)
+            throw error;
+        console.log('A new tuple inserted : ('+ req.body.googleId + ', ' + req.body.name + ', ' + req.body.age);
+        db.query(`SELECT * FROM user_info`, function (error, results, fields) {
+            if (error)
+                throw error;
+            res.send({
+                allOtherUsers: results,
+                mlResult: ["123", "456"]
+            });
+        });
+    });
 });

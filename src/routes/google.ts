bb04ad3b0e3cc88f1e239033a06c5615b6f27bca
KmGun <gunpoll823@gmail.com>
2022-11-13 16:26:04 +0900

    221113 google.ts - mysql : 로그인/회원가입 case 따라 redirection 되는 코드 수정

--- src/routes/google.ts
+++ src/routes/google.ts
@@ -16,12 +16,13 @@ googleRouter.get('/callback',
     function(req, res) {
         if(req.user){
             // 사용자의 이름, 나이 정보 유무 확인
-            db.query(`select EXISTS (select google_id from user_info where google_id=${req.user.id} AND (name=NULL OR age=NULL) limit 1) as success`,function (err,result){
-                if (result[0].success==0){
-                    // name 혹은 age 없는 경우, 이름/나이 입력 페이지로 이동
+            db.query(`select EXISTS (SELECT google_id FROM user_info WHERE google_id=? AND (name IS NULL OR age IS NULL) limit 1) as success`,[req.user.id],function (err,result){
+                console.log(result[0].success,req.user?.id)
+                if (result[0].success === 1){
+                    //처음 가입하는 유저 [ if google_id = req.user.id && (name || age = NULL) 일 경우] , 이름/나이 입력 페이지로 이동
                     res.redirect(`${process.env.CLIENT_URL}/auth/inputUserInfo/name`);
                 } else {
-                    // name 혹은 age 있는 경우, 매칭 페이지로 이동
+                    // 그외, matching 페이지로.
                     res.redirect(`${process.env.CLIENT_URL}/matching`)
                 }
             })

278430de56003552bfa9fcc7a27d5766a69091ab
KmGun <gunpoll823@gmail.com>
2022-11-18 20:22:47 +0900

    serialize/deserialize

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -1,14 +1,17 @@
 import passport from "passport";
+import { db } from "../server.js";
 import {google} from "./googleStrategy.js";
 
 export default function(){
     passport.serializeUser(function(data, done){
         console.log('serialize : ',data.id);
-        done(null, data);
+        done(null, data.id);
     })
-    passport.deserializeUser(function(data:Express.User, done){
-        console.log('deserialize :',data.id);
-        done(null, data);
+    passport.deserializeUser(function(userId, done){
+        db.query(`SELECT google_id FROM user_info WHERE google_id=?`,[userId],function(err,result){
+            console.log(`deserialize : ${result[0].google_id}`)
+            done(null, {id : result[0].google_id});
+        })
     })
     google();
 }
